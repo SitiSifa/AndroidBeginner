fad01f2cc879e00010fad09d118f8aba647fd3da
Firdaus Ekaputra <dex.weldes@gmail.com>
2017-04-27 14:04:44 +0700

    fixing and explain alert, button, onclick, implement

--- app/src/main/java/com/adfin/www/linearlayout/MainActivity.java
+++ app/src/main/java/com/adfin/www/linearlayout/MainActivity.java
@@ -6,71 +6,78 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+// Implement onClick listener karena terdapat banyak Button pada satu activity
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, DialogInterface.OnClickListener{
 
-    Context context = this;
-
-    Button alert1, alert2;
+//    Lebih baik dibuat satu-satu untuk definisi refrencenya biarpun Objectnya sama
+    private Button alert1, alert2, btnToast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button btnToast = (Button)findViewById(R.id.btnToast);
-        btnToast.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "Ini Toast", Toast.LENGTH_LONG).show();
-            }
-        });
+        btnToast = (Button)findViewById(R.id.btnToast);
+        //cara meliterasi onClick listener ke dalam 1 method OnClick
+        btnToast.setOnClickListener(this);
 
         alert1 = (Button)findViewById(R.id.btnAler1);
-        alert1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(context);
-                builder.setTitle("Ini Judul");
-                builder.setMessage("Ini Pesan");
-                builder.setNeutralButton("Close", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                }).show();
-            }
-        });
+        //cara meliterasi onClick listener ke dalam 1 method OnClick
+        alert1.setOnClickListener(this);
 
         alert2 = (Button)findViewById(R.id.btnAler2);
-        alert2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                AlertDialog.Builder alert2 = new AlertDialog.Builder(context);
+        //cara meliterasi onClick listener ke dalam 1 method OnClick
+        alert2.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        // Menggunakan switch case break untuk mengatasi button yang ada sehingga tidak terjadi kesalahan definisi
+        //dan membuat lebih simple coding akan lebih readable
+        switch (v.getId()){
+            case R.id.btnToast:
+                Toast.makeText(getApplicationContext(), "Ini Toast", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.btnAler1:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Ini Judul");
+                builder.setMessage("Ini Pesan");
+                builder.setNeutralButton("Close", this).show();
+                break;
+            case R.id.btnAler2:
+                AlertDialog.Builder alert2 = new AlertDialog.Builder(this);
                 alert2.setTitle("Warning");
                 alert2.setMessage("are you sure?");
-                alert2.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                    }
-                });
-                alert2.setNegativeButton("No", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
-                alert2.setNeutralButton("Cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-
-                    }
-                });
+                alert2.setPositiveButton("Yes", this);
+                alert2.setNegativeButton("No", this);
+                alert2.setNeutralButton("Cancel", this);
                 alert2.show();
-            }
-        });
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        Log.d("INI dialog", String.valueOf(which));
+        switch (which){
+            case -3:
+                dialog.dismiss();
+                break;
+            case -2:
+                dialog.dismiss();
+                break;
+            case -1:
+                Log.d("ALERT", "INI TOMBOL YES");
+                break;
+            default:
+                break;
+        }
     }
 }
